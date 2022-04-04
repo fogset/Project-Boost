f45f934bff4e044125cb95eb9f8bda3b4363a40e
Tianhao Zhang <zhangth1990@hotmail.com>
2022-04-04 04:32:08 -0700

    protect against nan error

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -14,7 +14,7 @@ public class Oscillator : MonoBehaviour
     float movementFactor;
 
     [SerializeField]
-    float period = 2f;
+    float period = 100f;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +26,10 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period <= Mathf.Epsilon)
+        {
+            return;
+        }
         float cycles = Time.time / period; // continually growing over time
         const float tau = Mathf.PI * 2; // constant value of 6.283
         float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
